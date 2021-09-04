7ffbfc669a0e583c348cfef2cbcd481fb95e0e7c
nutthanon <nutthanon.tho@gmail.com>
2021-09-04 15:10:55 +0700

    [+] ReactJS

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import App from "./App";
+import App from "../src/MainCompunent/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
