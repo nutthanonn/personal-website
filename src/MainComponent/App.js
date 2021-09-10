1b215f86acebce4c808a88c0560a3dc8d4a06f3c
nutthanon <nutthanon.tho@gmail.com>
2021-09-10 16:43:49 +0700

    [+] ReactJS

--- src/MainComponent/App.js
+++ src/MainComponent/App.js
@@ -1,7 +1,9 @@
+// import CardComponent from "./Login/cardComponent";
 import MainPage from "./mainPageComponent/MainPage";
 const App = () => {
   return (
     <div>
+      {/* <CardComponent /> */}
       <MainPage />
     </div>
   );
