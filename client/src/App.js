32ec77e1264a0fbcd0ce8729ace794f917fa0f68
nutthanon <nutthanon.tho@gmail.com>
2021-10-22 18:42:39 +0700

    [+] workshop

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,14 @@
-import { Route } from "react-router-dom";
-import { SnackbarProvider } from "notistack";
-import LoginPage from "./MainComponent/register_login/LoginPage";
-import RegisterPage from "./MainComponent/register_login/RegisterPage";
-import Home from "./MainComponent/register_login/Home";
-import MainPage from "./MainComponent/mainpage/MainPage";
+import { Router, Switch } from "react-router-dom";
+import Home from "./pages/Home";
 
-const App = () => {
+export default function App() {
   return (
-    <div>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/login">
-        <SnackbarProvider maxSnack={3}>
-          <LoginPage />
-        </SnackbarProvider>
-      </Route>
-      <Route path="/register">
-        <SnackbarProvider maxSnack={3}>
-          <RegisterPage />
-        </SnackbarProvider>
-      </Route>
-      <Route path="/test">
-        <MainPage />
-      </Route>
+    <div className="App">
+      <Switch>
+        <Router path="/">
+          <Home />
+        </Router>
+      </Switch>
     </div>
   );
-};
-
-export default App;
+}
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import Box from "@mui/material/Box";
+
+export default function Heom() {
+  return (
+    <Box>
+      <p>Hello</p>
+    </Box>
+  );
+}
