d31746bab1128c0b8054ecdcf322a749a75f340d
nutthanon <nutthanon.tho@gmail.com>
2022-04-19 19:04:34 +0700

    Restart

--- src/api/covidPageApi/timeLineCaseByProvince.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export async function fetchDataTimeLineCaseByProvince() {
-  const res = await axios.get(
-    "https://covid19.ddc.moph.go.th/api/Cases/timeline-cases-by-provinces"
-  );
-  return res.data;
-}
